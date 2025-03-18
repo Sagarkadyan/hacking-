e89073388c2f53720e1e286cbacd0c6c85f7dce5
Sagaradyan <sagarkadyan823@gmail.com>
2025-03-18 21:20:03 +0530

    Added C language project and updated assignment1.c

--- C language project /assignemt1.c
+++ C language project /assignemt1.c
@@ -1,1 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+long int n,i=1,sum=0,r;
+system("clear");
+printf("\nEnter the decimal number:");
+scanf("%ld",&n);
+while(n>0)
+{
+r=n%2;
+sum=sum+(r*i);
+i=i*10;
+n=n/2;    
+}   
+printf("\n Equivalent binary number is %ld",sum);
+getchar();
+}
