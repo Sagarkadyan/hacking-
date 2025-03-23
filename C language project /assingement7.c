cbe549e4e534d9ec72feae5b1811a0115579d71b
Sagaradyan <sagarkadyan823@gmail.com>
2025-03-23 19:58:01 +0530

    Updated assignment7.c

--- C language project /assingement7.c
+++ C language project /assingement7.c
@@ -4,9 +4,9 @@ void main()
 int n,i,j;
 printf("\nEnter the limit:");
 scanf("%d",&n);
-for (i=1;i <=n;i++);
+for (i=1;i <=n;i++)
 {
-for (j=2;j<i;j++);
+for (j=2;j<i;j++)
 {
 if(i%j==0)    
 break;
