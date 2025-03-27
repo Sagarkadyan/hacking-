381b7abaed8f0582124f25201db606fa42685cee
Sagaradyan <sagarkadyan823@gmail.com>
2025-03-28 05:04:05 +0530

    Updated assignment

---
+++ C language project /test.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    char op;
+    int n1;
+    int n2;
+    scanf("%c\n%d\n%d", &op, &n1, &n2);
+    // Don't change above this line
+    
+    // Write code here
+    
+    int res;
+    if ()
+
+    
+    // Don't change below this line
+    printf("%d", res);
+    return 0;
+};
