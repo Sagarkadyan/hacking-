868ded07e3963fe34314a7d44fd8aaf85e7511d7
Sagaradyan <sagarkadyan823@gmail.com>
2025-03-28 05:17:59 +0530

    Updated assignment

--- C language project /assingement6.c
+++ C language project /assingement6.c
@@ -1,4 +1,19 @@
- #include<stdio.h>
+/*
+AIM
+To write a C program to design a simple menu-driven calculator
+ALGORITHM
+STEP1.Start the program execution
+STEP2.Declare 2 variable a and b ,ch,c
+STEP3.Get choice 1.addition,2.subtraction,3.multiplaction,4.dividion,5.modulation
+STEP4.If ch=1,perform addition and print the result
+STEP5.If ch=2,perform subtration and print the result.
+STEP6.If ch=3,perform multiplaction and print the result.
+STEP7.If ch=4,perform dividion and print the result.
+STEP8.If ch=5,perform modulation and print the results
+STEP9.If choice is not in them print"Enter correct choice"
+STEP10.Stop thr program execution
+PROGRAM*/
+  #include<stdio.h>
  void  main()
  {
  int a,b,c,ch;
@@ -27,3 +42,6 @@
  }
  getchar();    
  }
+
+ /*
+ 
