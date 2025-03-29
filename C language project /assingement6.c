c7fba452d66edc4de3fd124c8fb77f370b04ce6e
Sagaradyan <sagarkadyan823@gmail.com>
2025-03-29 17:09:24 +0530

    Updated assignment

--- C language project /assingement6.c
+++ C language project /assingement6.c
@@ -43,5 +43,5 @@ PROGRAM*/
  getchar();    
  }
 
- /*
+ 
  
--- C language project /test.c
+++ C language project /test.c
@@ -1,19 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    char op;
-    int n1;
-    int n2;
-    scanf("%c\n%d\n%d", &op, &n1, &n2);
-    // Don't change above this line
-    
+   int sa,da,fa;
+   scanf("%d %d",&sa,&da);
+   fa=sa+da;
+   printf("%d",fa); 
     // Write code here
-    
-    int res;
-    if ()
-
-    
-    // Don't change below this line
-    printf("%d", res);
-    return 0;
+   return 0;
 };
