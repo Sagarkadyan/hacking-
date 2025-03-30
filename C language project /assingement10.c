b28ec7c6653d083cf2105e8c12104d32bee44c19
Sagaradyan <sagarkadyan823@gmail.com>
2025-03-30 10:39:31 +0530

    Updated assignment

--- C language project /assingement10.c
+++ C language project /assingement10.c
@@ -9,9 +9,33 @@ STEP4.Print the Fibonacci numbers
 STEP5.Stop the program execution
 PROGRAM*/
 #include<stdio.h>
+#include<stdlib.h>
 void fib(int);
 void main()
 {
 int n;
-sysyt    
+system("clear");
+printf("\nEnter the limit:");
+scanf("%d",&n);
+fib(n);
+getchar();
 }
+void fib(int n)
+{
+int i,a=0,b=1,c;
+printf("\nFibnocci series are:");
+printf("\n\n%d\t%d",a,b);
+for ( i = 3; i < n; i++)
+{
+    c=a+b;
+    a=b;
+    b=c;
+    printf("\t%d",c);
+}
+    
+}
+/*
+OUPUT
+Enter the the limit:5
+Fibonacci series are 
+0 1 1 3 5 */
