b33d0fba54131883e4682a35e5da7a197dd1cad0
Sagaradyan <sagarkadyan823@gmail.com>
2025-04-01 22:14:00 +0530

    Updated assignment

--- C language project /test2.c
+++ C language project /test2.c
@@ -1,18 +1,56 @@
-#include<stdio.h>
-#include<string.h>
-int main()
-{
-    
-    char str[100];
-    char ch;
-    int count = 0;
+//this calacular exit ehen you press e 
+#include <stdio.h>
+#include <stdlib.h>
 
-        while (1) {
-        scanf("%c", &ch);
-        if (ch == '$') break;  
-        if (ch != ' ') count++; 
+int main()  
+{
+    float sa, da, fa;
+    char operation;
+    char input[50];  // Buffer to hold the input
+    
+
+    while (1) {   
+        // Read the full line of input
+        if (fgets(input, sizeof(input), stdin) == NULL) {
+            break; // Exit if input fails
+        }
+
+        // Trim any trailing newline character
+        if (input[0] == 'e' && (input[1] == '\n' || input[1] == '\0')) {
+            break; // Exit if user enters 'e' alone
+        }
+
+        // Parse the operation and two numbers
+        if (sscanf(input, " %c %f %f", &operation, &sa, &da) != 3) {
+            continue; // Skip invalid inputs silently
+        }
+
+        // Perform the calculation based on the operation
+        switch (operation) {
+            case '+':
+                fa = sa + da;
+                printf("%f\n", fa);
+                break;
+            case '-':
+                fa = sa - da;
+                printf("%f\n", fa);
+                break;
+            case '*':
+                fa = sa * da;
+                printf("%f\n", fa);
+                break;
+            case '/':
+                if (da != 0) {
+                    fa = sa / da;
+                    printf("%f\n", fa);
+                } else {
+                    printf("Error: Division by zero is not allowed.\n");
+                }
+                break;
+            default:
+                continue; // Ignore invalid operations
+        }
     }
 
-    printf("%d\n", count);
     return 0;
-}   
+}
