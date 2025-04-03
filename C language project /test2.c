3010a69c5289a49218e3070e101409471d0476ce
Sagaradyan <sagarkadyan823@gmail.com>
2025-04-03 21:08:18 +0530

    Updated assignment

--- C language project /test2.c
+++ C language project /test2.c
@@ -1,56 +1,13 @@
-//this calacular exit ehen you press e 
-#include <stdio.h>
-#include <stdlib.h>
-
-int main()  
-{
-    float sa, da, fa;
-    char operation;
-    char input[50];  // Buffer to hold the input
-    
-
-    while (1) {   
-        // Read the full line of input
-        if (fgets(input, sizeof(input), stdin) == NULL) {
-            break; // Exit if input fails
-        }
-
-        // Trim any trailing newline character
-        if (input[0] == 'e' && (input[1] == '\n' || input[1] == '\0')) {
-            break; // Exit if user enters 'e' alone
-        }
-
-        // Parse the operation and two numbers
-        if (sscanf(input, " %c %f %f", &operation, &sa, &da) != 3) {
-            continue; // Skip invalid inputs silently
-        }
-
-        // Perform the calculation based on the operation
-        switch (operation) {
-            case '+':
-                fa = sa + da;
-                printf("%f\n", fa);
-                break;
-            case '-':
-                fa = sa - da;
-                printf("%f\n", fa);
-                break;
-            case '*':
-                fa = sa * da;
-                printf("%f\n", fa);
-                break;
-            case '/':
-                if (da != 0) {
-                    fa = sa / da;
-                    printf("%f\n", fa);
-                } else {
-                    printf("Error: Division by zero is not allowed.\n");
-                }
-                break;
-            default:
-                continue; // Ignore invalid operations
-        }
+#include<stdio.h>
+int main(){
+    // Write code here
+    int n1,n2;
+    scanf("%d%d",&n1,&n2);
+    if(n1>n2)
+     printf("%d",n1);
+    else
+    {
+      printf("%d",n2);
     }
-
-    return 0;
+    return 0; 
 }
