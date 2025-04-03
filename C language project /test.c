54ecccd3f462516d80234d370c895233d1311ea3
Sagaradyan <sagarkadyan823@gmail.com>
2025-04-03 14:32:01 +0530

    Updated assignment

--- C language project /test.c
+++ C language project /test.c
@@ -1,11 +1,31 @@
 #include <stdio.h>
 
 int main() {
-    // Write code here
-    int a,b,c,d,e;
-    float f;
-    scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
-    f=a+b+c+d+e/5;
-    
-    return 0;
-};
+  int num, digit, even=0,odd=0;
+
+  scanf("%d", &num);
+
+  
+
+  if (num == 0) {
+    printf("0\n");
+  } else {
+    while (num != 0) {
+      digit = num % 10;
+      if (digit % 2 ==0)
+      {
+         even = even+digit;
+      }
+      else
+      {
+         odd =odd+digit;
+      }
+      
+      
+      num /= 10;
+    }
+  }
+  printf("Sum even: %d\n",odd);
+  printf("Sum odd: %d\n",even);
+  return 0;
+}
