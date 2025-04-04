364e513408831767bd34b388ac2bb0555b48baa9
Sagaradyan <sagarkadyan823@gmail.com>
2025-04-04 18:58:08 +0530

    Updated assignment

--- C language project /test2.c
+++ C language project /test2.c
@@ -1,18 +1,11 @@
 #include <stdio.h>
 
 // Function to return the maximum of two numbers
-void max(int n1, int n2) {  // Function does not return a value (void)
+int main(int n1, int n2) {  // Function does not return a value (void)
+    scanf("%d %d", &n1, &n2);
     if (n1 > n2)
         printf("%d", n1);
     else
         printf("%d", n2);
-}
-
-int main() {
-    int n1, n2;
-    scanf("%d%d", &n1, &n2);  // Read input
-
-    max(n1, n2); // Call the function
-
-    return 0;
+     return 0;
 }
