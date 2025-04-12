d09dd9bace31234f24c9a49d885e922ba58f35b7
Sagaradyan <sagarkadyan823@gmail.com>
2025-04-12 20:33:30 +0530

    updated code

--- hextotext.py
+++ hextotext.py
@@ -1,11 +1,46 @@
-def print_range(start, end, step):
+'''def print_pattern(rows, cols):
     # Write your code here
-    pass
+    for i in range(rows):
+         print(('*' * cols +'\n') , end = '') 
+  
+    
 
-# Get input from user
-start = int(input())
-end = int(input())
-step = int(input())
+# Get input for rows and columns
+rows = int(input())
+cols = int(input())
 
 # Call the function
-print_range(start, end, step)
+print_pattern(rows, cols)'''
+'''a=int(input())
+
+c=0
+for i in range(a):
+    b=int(input())    
+    c+=b
+print(c)    
+a=int(input())
+b=0
+for i in range(1,10001,1):
+        b+=i
+def hell():
+  for j in range(a):
+   print(b)    
+hell()   
+def hell():
+        print(a*b)
+        
+a=int(input())
+b=int(input())
+hell()   
+b=1
+def square_number():
+        b=n**2
+        return b
+n=int(input())
+
+result = square_number()
+print(result)             '''
+def  sigma(n)
+  for i in range(1,n+1,1):
+          i+=i
+                  pass
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,13 +1,1 @@
-n1 = float(input())
-n2 = float(input())
-n3 = int(input())
-bill_amount=n1 
-
-
-tip_amount = (n2 / 100) * bill_amount
-total_amount = tip_amount + bill_amount
-split = total_amount /n3
-
-print("Bill Split Calculator")
-print(total_amount)
-print(split)
+   
