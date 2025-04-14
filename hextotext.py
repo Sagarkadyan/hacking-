5b1698f945225d38f12ff1ef78a4053e70d8c4ab
Sagaradyan <sagarkadyan823@gmail.com>
2025-04-14 15:48:18 +0530

    code

--- hextotext.py
+++ hextotext.py
@@ -62,10 +62,53 @@ choices=[3,4,5,6]
 get=random.choice(choices)
 print(get)
 shopping_list=["bread","eggs","milk","butter"]
-print(shopping_list)'''
-lst=["fdfddcd","greg","rrrdcdr","dvd"]
+print(shopping_list)
+lst = input().split(",")
+# Write your code below
 a=[]
+b=[]
 for i in range(0,len(lst)):
      
-       a[i]=len(lst[i])
-print(a)       
+       a.append(len(lst[i]))
+for j in range(0,len(a)):
+        if a[j]>5:
+          b.append(lst[j]) 
+                
+print(b)            
+
+text = input()
+b=0
+# Write your code below
+for i in range(len(text)):
+     text=text.lower()
+     if  text[i]=='p':
+      b+=1
+      print(text[i+1])
+     
+print(b)    
+text = input()
+delimiter = input()
+# Write your code below
+lst=text.split()
+lst=delimiter.join(lst)
+
+
+print(lst)'''
+lst = input().split(",")
+ls=[]
+a=len(lst)
+if  a==1:
+     ls.append(lst[0])  
+     print(ls) 
+elif a%2==0:
+  b=a//2
+  ls.append(lst[b-1])
+  ls.append(lst[b])
+  print(ls)
+else:
+  b=a//2
+  ls.append(lst[b-1])
+  ls.append(lst[b])
+  ls.append(lst[b+1])
+  
+  print(ls)     
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,9 @@
-lst=[1,2,3]
+text = input()
+b=0
+# Write your code below
+for i in range(len(text)):
+     text.lower()
+     if  text[i]=='p':
+      b+=1
+     
+print(b) 
