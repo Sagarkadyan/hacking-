1e10c932ff2cfa948fac32449c635781edbcdfad
Sagaradyan <sagarkadyan823@gmail.com>
2025-04-20 15:44:40 +0530

    code

--- Python/mysql.sql
+++
@@ -1,4 +0,0 @@
--- Select rows from a Table or View '
-
-SELECT * FROM SchemaName.
-WHERE 	/* add search conditions here */
--- Python/python.sql
+++
@@ -1,4 +0,0 @@
-python.-- Add a new column 'NewColumnName' to table 'TableName' in schema 'SchemaName'
-ALTER TABLE SchemaName.TableName
-    ADD NewColumnName /*new_column_name*/ int /*new_column_datatype*/ NULL /*new_column_nullability*/
-GO
