af68455f1d89699816d599f2d984610e5b045e11
Sagaradyan <sagarkadyan823@gmail.com>
2025-04-30 20:14:18 +0530

    code

--- C language project /assingement13.c
+++ C language project /assingement13.c
@@ -1,3 +1,30 @@
-git add .
-git commit -m "Updated assignment"
-git push origin main
+#include<stdio.h>
+//Selection sort implementation 
+void selectionSort(int arr[],int n){
+   for (int i = 0; i < n; i++)
+   {
+        int min =i;
+        for (int j = i+1; j < n; j++)
+        {
+            if(arr[j]<arr[min])
+             min =j;
+        }
+        if(min!=i){
+            int temp = arr[min];
+            arr[min]=arr[i];
+            arr[i]=temp;
+        }
+        
+   }
+   
+    
+}
+int main(){
+ int arr[]={2,6,1,5,3,4};
+ int n = sizeof(arr)/sizeof(arr[0]);
+ //Perform Selection Sort 
+ selectionSort(arr,n);
+ for(int i=0;i<n;i++) 
+   printf("%d",arr[i]);
+ return 0;     
+}
