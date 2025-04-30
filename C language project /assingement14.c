762f49a9fc09797d06628e1bc39f54ce7ca0d6b2
Sagaradyan <sagarkadyan823@gmail.com>
2025-04-30 21:43:13 +0530

    code

--- C language project /assingement14.c
+++ C language project /assingement14.c
@@ -1,2 +1,36 @@
 #include<stdio.h>
-#include<conio.h>
+#include<stdlib.h>
+void main()
+{
+   int c,pos;
+   FILE *fpl;
+   system("clear");
+   fpl=fopen("sudent.txt","r+");//the file has something to write in it
+   printf("\nReading from the file ...\n");
+   while(!feof(fpl))
+   {
+       c=fgetc(fpl);
+       printf("%c",c);
+
+   } 
+rewind(fpl);//position the pointer to the begging of the file 
+c=fgetc(fpl);
+printf("\nThe  first cgaracter from the file : % c",c);
+pos=ftell(fpl);//return th ecurrent position of the file pointer
+printf("\nThe position of the file pointer :%d",pos);
+fseek(fpl,10,SEEK_SET);//Moves the files pointer to the 10th byte  from the beginning
+pos=ftell(fpl);
+c=fgetc(fpl);
+printf("\nThe character at %d is %c  (from the beginning)",pos,c);
+fseek(fpl,-5,SEEK_CUR);// moves the file pointer back to the 5th byte
+//from the current position 
+pos=ftell(fpl);
+c=fgetc(fpl);
+printf("\n The character at %d id : %c",pos,c);
+fseek(fpl,12,SEEK_CUR);//moves the file pointer fordward to the 12 th
+//byte from the current position 
+pos=ftell(fpl);
+c=fgetc(fpl);
+printf("\nThe character at %d is :%c",pos,c);
+getchar();
+}
