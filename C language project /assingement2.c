4365c74b5de539d23a8cf5401cbde6d536f55e2e
Sagaradyan <sagarkadyan823@gmail.com>
2025-05-06 07:41:42 +0530

    Initial commit 1

--- C language project /assingement2.c
+++ C language project /assingement2.c
@@ -21,14 +21,14 @@ float r;
 system("clear");
 printf("\n Enter the choice:\n1.Simple intrest \n2.Comound intrest: \nchoice: ");
 scanf("%d",&ch);
-printf("\nEnter the value of amount number of years and rate of intrest:");
+printf("\nEnter the value of amount , number of years and rate of intrest:");
 scanf("%d%d%f",&p,&n,&r);
 switch(ch)
 {
 case 1: printf("\n The simple intrest is = %f", (p * n * r) / 100);
 break;
 
-case 2: printf("\n The compound intrest is = %f",(p *pow((1 +(r /100)),n)-p));
+case 2: printf("\n The compound intrest is = %f",(p * pow((1 +(r /100)),n)-p));
 break;
 default:printf("\nEnter the correct choice:");
 break;
---
+++ C language project /tempCodeRunnerFile.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void main()
+{
+int arr[10],i,n,sum=0;
+printf("\n enter the number of elements in the arry");
+// reading the size of the array
+scanf("%d",&n);
+printf("\n enter the values for the element of the array");
+for(i=0;i<n;i++);
+{
+   printf("\n enter the element %d",i+1);
+   scanf("%d",&arr[i]); 
+}
+for (i=0;i<n;i++);
+{
+    sum=sum+arr[i];
+}
+printf("\n printing the array elements .....");
+printf("\n the values for the elements of the array");
+for (i=0;i<n;i++);
+{
+    printf("\n the elements %d :%d",i+1,arr[i]);
+}
+printf("\n printing the array sum ...");
+printf("%d",sum);  
+}
