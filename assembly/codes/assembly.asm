c398ab0762c63196a5a4640891c976f4ce7b85ea
Sagaradyan <sagarkadyan823@gmail.com>
2025-05-10 21:43:08 +0530

    code

--- assembly/codes/assembly.asm
+++ assembly/codes/assembly.asm
@@ -1,2 +1,13 @@
-#x86 assembly
-.global _start
+;x86 assembl
+; first two lines define entry point for the proagram
+
+
+global  _start  ; global keyword is uded to make an identifier to linker
+; here start is an identifier
+_start:  ;colen after a identifier make a lael
+; lale ae used o name location
+  mov eax,1 ; moves integer  to eax reister 
+  mov ebx, 44 ; moves integer to ebx register  
+  sub ebx,29 ;suac value  
+  int 0x80
+  
